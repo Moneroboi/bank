37e20ab926df74667149c562770d32ff8994d9e2
Moneroboi <davidkylar@email.cz>
2024-07-22 09:49:58 +0200

    modified create_account-

--- create_account.cpp
+++ create_account.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "create_account.h"
 
-int main() {
+void create__account() {
+    int create_account_type;
+    
+    std::cout << "What type of account do you want to create?\n\n";
+
+    std::cout << "1) Basic\n";
+    std::cout << "2) Saving\n";
+    std::cout << "3) Investing\n";
+
+    std::cin >> create_account_type;
     
 }
